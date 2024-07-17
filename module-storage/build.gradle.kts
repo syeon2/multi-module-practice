aae44c2425b9028c4f3b5c8cfb51447bcaadee25
SUYEON KIM <waterkite94@gmail.com>
2024-07-17 23:54:38 +0900

    sub success

--- module-storage/build.gradle.kts
+++ module-storage/build.gradle.kts
@@ -1,7 +1,7 @@
 dependencies {
     implementation(project(":module-domain"))
 
-    api("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     runtimeOnly("com.mysql:mysql-connector-j")
 
     implementation("com.querydsl:querydsl-jpa:5.0.0:jakarta")
