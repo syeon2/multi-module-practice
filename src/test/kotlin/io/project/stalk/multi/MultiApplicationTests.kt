f7f0286c8351127540c95f5a6365fb384fe49f4e
SUYEON KIM <waterkite94@gmail.com>
2024-07-17 09:41:04 +0900

    testin

---
+++ src/test/kotlin/io/project/stalk/multi/MultiApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.project.stalk.multi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MultiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
